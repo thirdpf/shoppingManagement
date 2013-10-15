a613bd8b5a5a4deae2b0a805d3cbad4b4b4684ab
TIS505181 <TIS505181@XPA505181-T1.tisnt.tis.co.jp>
2013-10-15 13:33:22 +0900

    初期登録

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ShoppingManagement::Application.config.secret_key_base = '237d2ddee9b8d20ea9ac08606f3893602c6715b5b09936847200a99ac0e7071281c041a19d11362a2324dc964256d2144bb7dcec504945b9bf15515a1bab8e01'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ShoppingManagement::Application.config.session_store :cookie_store, key: '_shoppingManagement_session'
