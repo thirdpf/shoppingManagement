b8cb6f3fb3cce64807dae5a799a64beda74fe483
unknown <TIS505181@.tisnt.tis.co.jp>
2013-10-18 14:28:38 +0900

    shohi add2

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131017022458) do
+ActiveRecord::Schema.define(version: 20131018004922) do
 
   create_table "categories", force: true do |t|
     t.integer  "categorycd",   null: false
     t.string   "categoryname", null: false
     t.integer  "outturn",      null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "shohis", force: true do |t|
+    t.integer  "categoryCd"
+    t.string   "goods"
+    t.integer  "shohiSum"
+    t.datetime "shohiDate"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
